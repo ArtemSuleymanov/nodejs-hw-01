693b272c6420c7db367b5afde38f3c283346c7fc
Artem <suleymanov2007artem@gmail.com>
2025-03-22 16:31:11 +0200

    add countContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,8 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  const turnCountContacts = await readContacts();
+  return turnCountContacts.length;
+};
 
 console.log(await countContacts());
